b7e6bbb168823e6fb628043537333491c084e711
Kittinan Thatpaibool <2513110169@tni.ac.th>
2026-07-10 11:08:29 +0700

    commit

---
+++ LabSheet02/src/Ex06.java
@@ -0,0 +1,30 @@
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayController nums = new ArrayController();
+		nums.display("All elements");
+		
+		int[] new_nums = new int[nums.getSize()-1];
+		
+		int delete_index = 2;
+		
+		for(int i = 0; i<delete_index; i++) {
+			new_nums[i] = nums.getNums()[i];
+		}
+		
+		for(int i = delete_index+1; i<nums.getSize();i++) {
+			new_nums[i-1] = nums.getNums()[i];
+		}
+		
+		nums.setNums(new_nums);
+		nums.display("\nDeleted data index 2:");
+		
+		
+		
+		
+	}
+
+}
